8bbede42520814d4f0d4299704b21dcd4f1cef32
zabuzabuzaza <i.am.deon.chen@gmail.com>
2020-09-05 17:38:30 +1000

    changes

---
+++ serial_case/main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Serial version
+"""
+import sys
+import serial 
+import wx 
+
+from arduino import Arduino
+from dataset import DataSet
+from interface import Gooey
+import util
+
+
+
+def main(): 
+    
+    ex = wx.App()
+    Gooey(None, "My Gooey")
+    ex.MainLoop()
+     
+
+    
+    # newArduino = Arduino()
+    # ser = newArduino.ser
+    
+    
+    # newDataset = DataSet() 
+    
+
+if __name__ == '__main__': 
+    try: 
+        main()
+    except Exception as e:
+        #tempArduino = serial.Serial('COM3')
+        #tempArduino.close()
+        print(e)
+        sys.exit(0)
