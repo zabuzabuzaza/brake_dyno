5bb429d19a668836227a4007abaca83d22c1a019
zabuzabuzaza <i.am.deon.chen@gmail.com>
2020-09-05 22:45:53 +1000

    Upgraded GUI

--- interface.py
+++ interface.py
@@ -1,74 +1,115 @@
 # -*- coding: utf-8 -*-
 """
-GUI class to handle interations and whatnot. 
+GUI class to handle interations and whatnot.
 """
 
 from dataset import DataSet
 from arduino import Arduino
 import util
 import wx
+import wx.xrc
 
-class Gooey(wx.Frame): 
+class Gooey(wx.Frame):
     def __init__(self, parent, title):
-        """
-        Initialises the main GUI frame for all user interation and event 
-        handling. 
+        # """
+        # Initialises the main GUI frame for all user interation and event
+        # handling.
 
-        Parameters
-        ----------
-        parent : Frame
-            a parent frame in which to initialise in. Usually 'None'. 
-        title : string
-            a title for the window. 
-        """
-        super(Gooey, self).__init__(parent, title=title, size=(200,150))
-        self.SetTitle(title)
-        self.initUI()
-        
-    def initUI(self): 
-        """
-        Populates the frame with the required components and functionality. 
+        # Parameters
+        # ----------
+        # parent : Frame
+        #     a parent frame in which to initialise in. Usually 'None'.
+        # title : string
+        #     a title for the window.
+        # """
+        # super(Gooey, self).__init__(parent, title=title, size=(200,150))
+        # self.SetTitle(title)
+        # self.initUI()
+        super(Gooey, self).__init__(parent, title = title, size = ( 700,250 ))
 
-        Returns
-        -------
-        None.
-        """
-        panel = wx.Panel(self) 
-        
-        self.but2 = wx.Button(panel, label="Record Data")
-        self.but2.Bind(wx.EVT_BUTTON, self.executeAcq)
-        
+        panel = wx.Panel(self)
+
+        sizer1 = wx.BoxSizer(wx.VERTICAL)
+
+        self.staticText1 = wx.StaticText(panel, label="Welcome to our Brake Dyno", style=wx.ALIGN_CENTRE)
+        sizer1.Add(self.staticText1, 0, wx.ALIGN_CENTRE, 5)
+
+        sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.staticText2 = wx.StaticText(panel, label="Enter test length (millises)")
+        sizer2.Add(self.staticText2, 0, wx.ALIGN_CENTER, 5)
+        self.enterText = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER)
+        sizer2.Add(self.enterText, 0, wx.ALIGN_CENTER, 5)
+        self.test_length = 2000
+
+        sizer1.Add(sizer2, 0, wx.ALIGN_CENTER, 5)
+
+
+        self.but1 = wx.Button(panel, label="Record Data")
+        sizer1.Add(self.but1, 0, wx.ALIGN_CENTER, 5)
+
+
+
+        panel.SetSizer(sizer1)
+
+        #self.Layout()
         self.Centre()
-        self.Show(True)
-        
-    def onClicked(self, event): 
+        self.Show()
+
+        self.but1.Bind(wx.EVT_BUTTON, self.executeAcq)
+        self.enterText.Bind(wx.EVT_TEXT_ENTER, self.getTestLength)
+
+
+    # def initUI(self):
+    #     """
+    #     Populates the frame with the required components and functionality.
+
+    #     Returns
+    #     -------
+    #     None.
+    #     """
+    #     panel = wx.Panel(self)
+
+    #     self.but2 = wx.Button(panel, label="Record Data")
+    #     self.but2.Bind(wx.EVT_BUTTON, self.executeAcq)
+
+    #     self.Centre()
+    #     self.Show(True)
+
+    def onClicked(self, event):
         """
         DEPRECATED. It's only here so I don't keep forgetting the structure
-        for an event handler. 
+        for an event handler.
         """
-        buttonLabel = event.GetEventObject().GetLabel() 
+        buttonLabel = event.GetEventObject().GetLabel()
         print(buttonLabel)
-        
-    def onMouseMove(self, e): 
+
+    def onMouseMove(self, e):
         """
         DEPRECATED. It's only here so I don't keep forgetting the structure
-        for an event handler. 
+        for an event handler.
         """
         x, y = e.GetPosition()
         print(f"Current mouse position x = {x}, y = {y}")
-        
-    def executeAcq(self, event): 
+
+    def getTestLength(self, event):
+        test_time = int(event.GetEventObject().GetLineText(0))
+        self.test_length = test_time
+        self.but1.SetLabel(f"Start Recording {test_time}")
+        print(self.test_length)
+
+    def executeAcq(self, event):
         """
         Opens the serial port and starts the process of:
-            - reading the serial port 
+            - reading the serial port
             - data recording
             - saving data to a csv file
-        Closes the serial when done. 
+        Closes the serial when done.
 
         Parameters
         ----------
         event : event handler
-            A reference to the action that triggered this function. 
+            A reference to the action that triggered this function.
 
         Returns
         -------
@@ -78,13 +119,16 @@ class Gooey(wx.Frame):
         # opens serial connection
         newArduino = Arduino()
         ser = newArduino.ser
-        
+
         # creates a Dataset object to retrieve and store the data
-        newDataset = DataSet() 
+        newDataset = DataSet()
         newDataset.runDataAcq(ser)
-        data = newDataset.dataset    
-        
+        data = newDataset.dataset
+
         # takes the Dataset ojbect and saves its contents to file
         util.data2csv(data)
-        
+
         ser.close()
+
+
+
--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Data aquisition software for the 2020 MECH4552 Brake Dyno team. 
+Data aquisition software for the 2020 MECH4552 Brake Dyno team.
 """
 import sys
-import wx 
+import wx
 
 from interface import Gooey
 
 
-def main(): 
+def main():
     """
-    Entry point to data aquisition software. Initialises the wxPython frame 
-    and runs the event handling loop. 
+    Entry point to data aquisition software. Initialises the wxPython frame
+    and runs the event handling loop.
     """
-    
+
     ex = wx.App()
     Gooey(None, "My Gooey")
     ex.MainLoop()
-     
-    
 
-if __name__ == '__main__': 
-    try: 
+
+
+if __name__ == '__main__':
+    try:
         main()
     except Exception as e:
-        # haven't been able to get the serial port ot properly close in the 
+        # haven't been able to get the serial port ot properly close in the
         # event of an Exception. It's not serious or anything, just have to
-        # unplug it and plug the Arduino back in every time an Exception 
+        # unplug it and plug the Arduino back in every time an Exception
         # happens
-        
+
         # tempArduino = serial.Serial('COM3')
         # tempArduino.close()
         print(e)
