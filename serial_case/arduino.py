e91c37ed4116144c5c887c439028c033ade6216a
zabuzabuzaza <i.am.deon.chen@gmail.com>
2020-09-05 19:02:14 +1000

    Added "COM3" argument

--- serial_case/arduino.py
+++ serial_case/arduino.py
@@ -6,12 +6,24 @@ import serial
 
 
 class Arduino: 
-    def __init__(self): 
+    def __init__(self, port='COM3'): 
         """
         Initialises the serial connection for communication between Arduino 
         & computer. 
+
+        Parameters
+        ----------
+        port : string, optional
+            The port used by the Arduino. Can be checked from the Arduino IDE 
+            or the Device Manager. 
+            The default is 'COM3'.
+
+        Returns
+        -------
+        None.
+
         """
-        self.ser = serial.Serial('COM3')
+        self.ser = serial.Serial(port)
         
 
     
